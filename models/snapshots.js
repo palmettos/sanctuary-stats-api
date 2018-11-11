96c634acbba41ca41302094865b8fedb5a1ffa7a
palmettos <nyoung0@gmail.com>
2018-11-10 22:38:25 -0500

    add missing field

--- models/snapshots.js
+++ models/snapshots.js
@@ -4,6 +4,7 @@ let itemSchema = new mongoose.Schema({
     uniqueName: {type: String, required: true},
     baseName: {type: String, required: true},
     quality: {type: String, required: true},
+    location: {type: String, required: true},
     properties: [{type: String, required: false}]
 });
 
