9ad2c754447177289b95716ad452f8ede6c7df1a
palmettos <nyoung0@gmail.com>
2018-11-17 23:33:27 -0500

    testing writing item state to db

--- controllers/snapshots.js
+++ controllers/snapshots.js
@@ -1,6 +1,7 @@
 const snapshots = require('../models/snapshots');
 const logger = require('../logger');
 const mongoose = require('mongoose');
+const {equippedItemsSchema} = require('../models/snapshots');
 
 
 exports.getEquippedItems = function(req, res) {
@@ -9,5 +10,22 @@ exports.getEquippedItems = function(req, res) {
 
 exports.updateEquippedItems = function(req, res) {
     logger.debug('In processEquippedItems handler.');
+    logger.info(JSON.stringify(req.body, null, 2));
+    equippedItemsSchema.findOneAndUpdate(
+        {
+            channel: req.body.channel,
+            characterName: req.body.characterName,
+            characterClass: req.body.characterClass,
+            characterLevel: req.body.characterLevel,
+        },
+        req.body,
+        {upsert: true, new: true},
+        (err, doc, res) => {
+            if (err) {
+                logger.error(`Error writing equipped snapshot to db: ${err}`);
+            } else {
+                logger.info(`Successfully wrote to db: ${doc}`);
+            }
+        });
     res.send('Snapshots will be sent here by streamers');
 }
--- models/snapshots.js
+++ models/snapshots.js
@@ -2,19 +2,22 @@ const mongoose = require('mongoose');
 
 
 let itemSchema = new mongoose.Schema({
+    //uid: {type: Number, required: true},
     uniqueName: {type: String, required: true},
     baseName: {type: String, required: true},
     quality: {type: String, required: true},
+    //page: {type: String, required: true},
     location: {type: String, required: true},
     properties: [{type: String, required: false}]
 });
 
 let equippedItemsSchema = new mongoose.Schema({
-    streamer: {type: String, required: true},
-    character: {type: String, required: true},
     timestamp: {type: Number, required: true},
-    equipment: [itemSchema],
-    charms: [itemSchema]
+    channel: {type: String, required: true},
+    characterName: {type: String, required: true},
+    characterClass: {type: String, required: true},
+    characterLevel: {type: Number, required: true},
+    payload: [itemSchema]
 });
 
-exports.equippedItems = mongoose.model('equippedItems', equippedItemsSchema);
+exports.equippedItemsSchema = mongoose.model('equippedItems', equippedItemsSchema);
