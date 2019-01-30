634b8a5a6e9eebb4ba5f6d550e68bbe2f92f23df
palmettos <nyoung0@gmail.com>
2019-01-29 19:54:14 -0500

    some simple documentation

---
+++ README.md
@@ -0,0 +1,13 @@
+Sanctuary Stats API
+===
+
+Motivation
+---
+
+Diablo II: Lord of Destruction speedruns have received increasing viewership on Twitch in recent years due to their highly entertaining and dynamic nature. Much information about the state of a speedrun, however, is hidden in game menus that the viewer rarely has a chance to observe. By exposing this information to the viewer in real-time, we can dramatically improve the experience of spectating a speedrun. 
+
+Previously, I built a Twitch extension called Diablo 2 Item Display. This extension only displays the runner's current equipped items. The Sanctuary Stats API is the basis for the functionality of the next iteration of the Twitch extension. My goal is to increase the amount of game state that's exposed to the viewer as well as build a system that's more robust, better documented and easier to extend. The data is recorded in a database at certain intervals and served by the API as time series. This allows changes in the data over time to be visualized in the extension's front-end. This makes it easier to estimate the quality of a run if the viewer missed the beginning of it, as well as enriching the viewer's experience overall.
+
+Overview
+---
+The sequence of events involved in sending data from the broadcaster's system to the extension front-end is as follows: [transaction]: 
--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const authRouter = require('./routes/auth');
 const passport = require('passport');
 
 
-mongoose.connect(`mongodb://${config.db_user}:${config.db_pass}@${config.db_addr}`, {useNewUrlParser: true})
+mongoose.connect(
+    `mongodb://${config.db_user}:${config.db_pass}@${config.db_addr}`,
+    {useNewUrlParser: true}
+)
     .then(() => {
         logger.info('Connected to the database.');
         startServer();
@@ -19,24 +22,36 @@ mongoose.connect(`mongodb://${config.db_user}:${config.db_pass}@${config.db_addr
     });
 
 function startServer() {
+    // Initialize the router
     let app = express();
     let v1 = express.Router();
     
+    // Set CORS header
     v1.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         next();
     });
+
+    // Initialize endpoints
     v1.use('/snapshots', snapshotsRouter);
     v1.use('/auth', authRouter);
     
+    // Initialize authentication modules
     app.use(passport.initialize());
     require('./auth/init');
+
+    // Initialize body parser
     app.use(bodyParser.json());
+
+    // Append everything to the v1 URL
     app.use('/api/v1', v1);
+
+    // Respond with 404 to any request that falls through
     app.all('*', function(req, res) {
         res.sendStatus(404);
     });
 
+    // Listen
     app.listen(config.port, () => {
         logger.info(`Listening on port ${config.port}.`);
     });
